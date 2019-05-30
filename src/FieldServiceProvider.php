1b6f84499ed3d57a938cd6260629337605d87a7f
Tarvo Reinpalu <tarvoreinpalu@gmail.com>
2019-05-30 13:33:48 +0300

    Add config publish

--- src/FieldServiceProvider.php
+++ src/FieldServiceProvider.php
@@ -19,6 +19,10 @@ class FieldServiceProvider extends ServiceProvider
             Nova::script('nova-locale-field', __DIR__ . '/../dist/js/field.js');
             Nova::style('nova-locale-field', __DIR__ . '/../dist/css/field.css');
         });
+
+        $this->publishes([
+            __DIR__ . '/../config/nova-locale-field.php' => config_path('nova-locale-field.php'),
+        ], 'config');
     }
 
     /**
