bdbc1907fd5432e792a52b990a47319d6e8650b3
Tarvo Reinpalu <tarvoreinpalu@gmail.com>
2021-02-25 16:30:09 +0200

    Remove empty CSS file

--- src/FieldServiceProvider.php
+++ src/FieldServiceProvider.php
@@ -17,7 +17,6 @@ class FieldServiceProvider extends ServiceProvider
     {
         Nova::serving(function (ServingNova $event) {
             Nova::script('nova-locale-field', __DIR__ . '/../dist/js/field.js');
-            Nova::style('nova-locale-field', __DIR__ . '/../dist/css/field.css');
         });
 
         $this->publishes([
